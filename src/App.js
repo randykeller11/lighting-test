3a868c4cac2f71bf51e3ee86ca936ddc2af18820
Randy <randykeller1193@gmail.com>
2022-04-13 11:13:07 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useRef } from "react";
+import { Canvas } from "@react-three/fiber";
+import Guitar from "./components/Guitar";
+import { OrbitControls, TransformControls } from "@react-three/drei";
+import { Controls, useControl } from "react-three-gui";
+
+function Keen({ props }) {
+  const orbit = useRef();
+  const transform = useRef();
+  const mode = useControl("mode", {
+    type: "select",
+    items: ["scale", "rotate", "translate"],
+  });
+  useEffect(() => {
+    if (transform.current) {
+      const controls = transform.current;
+      controls.setMode("translate");
+      const callback = (event) => (orbit.current.enabled = !event.value);
+      controls.addEventListener("dragging-changed", callback);
+      return () => controls.removeEventListener("dragging-changed", callback);
+    }
+  });
+  return (
+    <>
+      <TransformControls ref={transform}>
+        <pointLight intensity={0.5} />
+      </TransformControls>
+      <OrbitControls ref={orbit} />
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="canvas">
+      <Canvas>
+        <Keen />
+        <Guitar
+          scale={[0.5, 0.5, 0.5]}
+          rotation={[0, Math.PI / 2, Math.PI / 2]}
+        />
+      </Canvas>
     </div>
   );
 }
